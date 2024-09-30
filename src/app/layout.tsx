975ff872ad42d8528e76b1de7b5a431bda742153
JosehGaks <josehwilddog@gmail.com>
2024-09-30 18:00:52 +0300

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
-import ToasterContext from "./api/contex/ToasetContex";
 import { useEffect, useState } from "react";
 import PreLoader from "@/components/Common/PreLoader";
+import ToasterContext from "./api/contex/ToasetContex";
 
 export default function RootLayout({
   children,
@@ -38,7 +38,7 @@ export default function RootLayout({
             <ThemeProvider
               attribute="class"
               enableSystem={false}
-              defaultTheme="light"
+              defaultTheme="dark"
             >
               <ToasterContext />
               <Header />
